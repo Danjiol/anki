319381f080645af3c69bcb4d58d1b2660a247fc4
Ewert <dewert@awo-mitte.local>
2025-01-13 21:39:28 +0100

    Add PWA functionality

---
+++ public/serviceWorker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  // Fügen Sie hier weitere Assets hinzu, die Sie cachen möchten
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+}); 
